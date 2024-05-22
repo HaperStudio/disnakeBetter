1d67d9873c53ec79b7026f114f2ae18e35dc9469
Jupiter404E <92510876+Jupiter404E@users.noreply.github.com>
2024-05-22 13:55:56 +0200

    Update __init__.py

--- disnakeBetter/ext/__init__.py
+++ disnakeBetter/ext/__init__.py
@@ -1,17 +1,17 @@
 """
 
-Copyright (C) 2024 Jupiter404E.
+Copyright (C) 2024 HåperStudio.
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-:copyright: (c) 2024 present Jupiter404E
+:copyright: (c) 2024 present HåperStudio
 :license: MPL-2.0 license, see LICENSE for more details.
 
 """
 
 __title__ = 'disnakeBetter'
-__author__ = 'Jupiter404E'
+__author__ = 'HåperStudio'
 __license__ = 'MPL-2.0 license'
-__copyright__ = 'Copyright 2024 present Jupiter404E'
+__copyright__ = 'Copyright 2024 present HåperStudio'
 __version__ = '0.2'
 
 __all__ = [
