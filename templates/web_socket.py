1d81dbd23032458728a12cecdae80579d3b6568b
Jupiter404E <92510876+Jupiter404E@users.noreply.github.com>
2024-05-22 14:01:44 +0200

    Create web_socket.py

---
+++ templates/web_socket.py
@@ -0,0 +1,29 @@
+import disnake, sys
+from disnake.ext import commands
+from disnake.gateway import DiscordWebSocket
+
+# Import disnakeBetter
+import disnakeBetter
+from disnakeBetter.ext import BotWebSocket
+
+# Set bot configs
+intents = disnake.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix = '!', intents = intents)
+
+# Testing command
+@bot.command()
+async def ping(inter: disnake.AppCmdInter):
+    await inter.send("Pong!")
+
+@bot.event
+async def on_ready():
+    print(f'Bot ready: {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+# Starting bot
+if __name__ == '__main__':
+    DiscordWebSocket.identify = BotWebSocket.status_mobile # Set custom bot web socket preset. For example mobile status
+  
+    bot.run("") # Bot token
